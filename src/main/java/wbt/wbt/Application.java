3cd5232b1f3f9cd537bbd8fd5e8c432a04922568
Diana-Georgiana Mocanu <dianagmocanu@gmail.com>
2020-03-18 08:43:33 +0200

    first think

---
+++ src/main/java/wbt/wbt/Application.java
@@ -0,0 +1,9 @@
+package wbt.wbt;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello wbt");
+    }
+
+}
---
+++ src/main/java/wbt/wbt/app/MainApplication.java
@@ -0,0 +1,36 @@
+package wbt.wbt.app;
+
+
+import wbt.wbt.repository.NotaXMLRepo;
+import wbt.wbt.repository.StudentXMLRepo;
+import wbt.wbt.repository.TemaXMLRepo;
+import wbt.wbt.service.Service;
+import wbt.wbt.validation.NotaValidator;
+import wbt.wbt.validation.StudentValidator;
+import wbt.wbt.validation.TemaValidator;
+import wbt.wbt.view.UI;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+       //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+
+}
